10c91037e4e35191d7b40d4f2488f569b0896bfa
robokon <robokon_ksk@outlook.com>
2017-06-01 01:00:59 +0900

    Basic Design
    暫定で申し訳ありませんが，基本設計として作成したファイルをアップロードします．
    各難所毎のファイルとタスクに分けてますので，各チームでファイルの自由に修正してください．(ファイル名等も自由に変えてください．)
    タスクをどのように処理させるのかも，詰め切れてませんので，基本設計を変更する可能性があります．

---
+++ sample_c4/garage.h
@@ -0,0 +1,21 @@
+/*
+ * garage.h
+ */
+
+#ifndef _GARAGE_H_
+#define _GARAGE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+extern void garage_func();
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* _GARAGE_H_ */
+
