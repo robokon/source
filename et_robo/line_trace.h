02a6e63608885d14bba29742b9046deb9a7d722c
robokon <robokon_ksk@outlook.com>
2017-06-10 08:16:21 +0900

    Basic Design fix
    渡邉さんの指摘を反映致しました．
    また，共通部(common.h)を作成し，戻り値等を定義しております．(要不要は検討)
    
    【渡邉さん指摘内容】
    ・難所毎のタスク制御が必要か疑問
    ・スタート後，4msおきにループで回している部分内の処理を関数に切り出し，機能の個数分同じものを作る．
    　(4msおきにコールされる前提，倒立制御もモーター制御も全部自分でやる．変数保持が必要なら自分でやる)
    ・機能の個数分状態を用意
    ・状態に応じてswitchで呼び分ける

---
+++ et_robo/line_trace.h
@@ -0,0 +1,22 @@
+/*
+ * line_tarce.h
+ * 
+ */
+#ifndef _LINE_TRACE_H_
+#define _LINE_TRACE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "common.h"
+
+extern void line_tarce_main();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _LINE_TRACE_H_ */
+
+/* end of file */
---
+++ et_robo/look_up_gate.h
@@ -0,0 +1,23 @@
+/*
+ * look_up_gate.h
+ */
+
+#ifndef _LOOK_UP_GATE_H_
+#define _LOOK_UP_GATE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "common.h"
+
+extern void look_up_gate_main();
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* _LOOK_UP_GATE_H_ */
+
+/* end of file */
