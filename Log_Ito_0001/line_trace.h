17f171a42dc604454dd5f4262701f8af354125d6
RyotaIto <jo.jo.jo.george.ri.04@ezweb.ne.jp>
2017-06-16 21:26:48 +0900

    伊藤　テスト

---
+++ Log_Ito_0001/line_trace.h
@@ -0,0 +1,20 @@
+/*
+ * line_tarce.h
+ * 
+ */
+#ifndef _LINE_TRACE_H_
+#define _LINE_TRACE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+extern void line_tarce_func();
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* _LINE_TRACE_H_ */
