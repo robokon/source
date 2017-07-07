b5846cd915a8ab65b2132ede1f802523b9240f09
robo <robokon_ksk@outlook.com>
2017-07-08 00:30:23 +0900

    走行距離検出追加

--- Log_Ito_0001/app.c
+++ Log_Ito_0001/app.c
@@ -11,6 +11,8 @@
 #include "ev3api.h"
 #include "app.h"
 #include "balancer.h"
+#include "Distance.h"
+#include "common.h"
 
 #if defined(BUILD_MODULE)
 #include "module_cfg.h"
@@ -26,48 +28,11 @@
 #define _debug(x)
 #endif
 
-/**
- * センサー、モーターの接続を定義します
- */
-static const sensor_port_t
-    touch_sensor    = EV3_PORT_1,
-    sonar_sensor    = EV3_PORT_2,
-    color_sensor    = EV3_PORT_3,
-    gyro_sensor     = EV3_PORT_4;
-
-static const motor_port_t
-    left_motor      = EV3_PORT_C,
-    right_motor     = EV3_PORT_B,
-    tail_motor      = EV3_PORT_A;
-
 static int      bt_cmd = 0;     /* Bluetoothコマンド 1:リモートスタート */
 static FILE     *bt = NULL;     /* Bluetoothファイルハンドル */
 
 static int LIGHT_WHITE=0;         /* 白色の光センサ値 */
 static int LIGHT_BLACK=100;          /* 黒色の光センサ値 */
-
-/* 下記のマクロは個体/環境に合わせて変更する必要があります */
-/* sample_c1マクロ */
-#define GYRO_OFFSET  0          /* ジャイロセンサオフセット値(角速度0[deg/sec]時) */
-
-/* sample_c2マクロ */
-#define SONAR_ALERT_DISTANCE 30 /* 超音波センサによる障害物検知距離[cm] */
-/* sample_c3マクロ */
-#define TAIL_ANGLE_STAND_UP  85 /* 完全停止時の角度[度] */
-#define TAIL_ANGLE_START     98 /* スタート時の角度[度] */
-#define TAIL_ANGLE_DRIVE      3 /* バランス走行時の角度[度] */
-#define P_GAIN             2.5F /* 完全停止用モータ制御比例係数 */
-#define PWM_ABS_MAX          60 /* 完全停止用モータ制御PWM絶対最大値 */
-/* sample_c4マクロ */
-//#define DEVICE_NAME     "ET0"  /* Bluetooth名 hrp2/target/ev3.h BLUETOOTH_LOCAL_NAMEで設定 */
-//#define PASS_KEY        "1234" /* パスキー    hrp2/target/ev3.h BLUETOOTH_PIN_CODEで設定 */
-#define CMD_START         '1'    /* リモートスタートコマンド */
-
-/* LCDフォントサイズ */
-#define CALIB_FONT (EV3_FONT_SMALL)
-#define CALIB_FONT_WIDTH (6/*TODO: magic number*/)
-#define CALIB_FONT_HEIGHT (8/*TODO: magic number*/)
-
 
 #if (LOG_TASK == TASK_ON)
 /* Log の最大回数 */
@@ -96,16 +61,10 @@ int LogNum = 0;
 Logger gst_Log_str[LOG_MAX]; /* (1s == 250) */
 #endif
 
-/* 関数プロトタイプ宣言 */
-static int sonar_alert(void);
-static float tail_control(signed int angle);
-
-
 #if (LOG_TASK == TASK_ON)
 void log_str(uint8_t reflect, int16_t rate, int16_t turn,int16_t p,int16_t d);
 void log_commit(void);
 #endif
-
 
 /* メインタスク */
 void main_task(intptr_t unused)
@@ -136,6 +95,8 @@ void main_task(intptr_t unused)
 
     ev3_led_set_color(LED_ORANGE); /* 初期化完了通知 */
 
+	Distance_init(); /* 距離計測変数初期化 */
+	
     /*白色の光センサ値取得*/
     while(1)
     {
@@ -262,7 +223,7 @@ void log_create_task(intptr_t idx)
 // 返り値 : 1(障害物あり)/0(障害物無し)
 // 概要 : 超音波センサによる障害物検知
 //*****************************************************************************
-static int sonar_alert(void)
+int sonar_alert(void)
 {
     static unsigned int counter = 0;
     static int alert = 0;
@@ -297,7 +258,7 @@ static int sonar_alert(void)
 // 返り値 : 無し
 // 概要 : 走行体完全停止用モータの角度制御
 //*****************************************************************************
-static float tail_control(signed int angle)
+float tail_control(signed int angle)
 {
     float pwm = (float)(angle - ev3_motor_get_counts(tail_motor))*P_GAIN; /* 比例制御 */
     /* PWM出力飽和処理 */
@@ -502,10 +463,22 @@ void line_trace_task(intptr_t unused)
         ev3_motor_set_power(right_motor, (int)pwm_R);
     }
 
-	if(ev3_button_is_pressed(BACK_BUTTON))
-	{
-		wup_tsk(MAIN_TASK);
-	}
+    Distance_update(); /* 移動距離加算 */
+    
+    if( Distance_getDistance() > DISTANCE_NOTIFY )
+    {
+        /* DISTANCE_NOTIFY以上進んだら音を出す */
+        ev3_speaker_set_volume(100); 
+        ev3_speaker_play_tone(NOTE_C4, 100);
+        
+        /* 距離計測変数初期化 */
+        Distance_init();
+    }
+    
+    if(ev3_button_is_pressed(BACK_BUTTON))
+    {
+        wup_tsk(MAIN_TASK);
+    }
     if(gyro < -150 || 150 < gyro)
     {
         wup_tsk(MAIN_TASK);
