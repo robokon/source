3f6017551364456dd092629326db0754d4416f7a
RyotaIto <jo.jo.jo.george.ri.04@ezweb.ne.jp>
2017-07-22 10:41:47 +0900

    試走会のときのログ取りで使用していたもの

--- et_robo/app.c
+++ et_robo/app.c
@@ -15,6 +15,8 @@
 
 #include "line_trace.h"
 #include "Distance.h"
+#include "stair.h"
+#include "look_up_gate.h"
 
 #if defined(BUILD_MODULE)
 #include "module_cfg.h"
@@ -96,7 +98,7 @@ void main_task(intptr_t unused)
     ev3_led_set_color(LED_GREEN); /* スタート通知 */
     
     /* スタート通知後、通常のライントレースに移行するように設定 */
-    main_status = STAT_NORMAL; 
+    main_status = STAT_STAIR; 
 
     /**
     * Main loop for the self-balance control algorithm
@@ -112,7 +114,7 @@ void main_task(intptr_t unused)
 
             /* 階段制御中 */
             case STAT_STAIR:
-                /* T.B.D */
+                stair_main();
                 break;
 
             /* ルックアップゲート制御中 */
